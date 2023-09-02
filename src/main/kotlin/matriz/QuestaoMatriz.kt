a2e5db6a730e038addc31320ad455019aaac4cad
Alberto Rodrigues <allio.professional@gmail.com>
2023-09-02 00:25:24 -0300

    primeiro commit, fazendo a parte de matrizes deo código do processo de seleção Ceos

---
+++ src/main/kotlin/matriz/QuestaoMatriz.kt
@@ -0,0 +1,73 @@
+package matriz
+
+import java.io.File
+import java.io.FileNotFoundException
+
+fun main() {
+   ler() //utilize C:/Users/alber/IdeaProjects/CeosProcessoSeletivo/out/production/CeosProcessoSeletivo/lista.txt
+}
+
+//seleciona o arquivo a ser lido e então chama as funções necessárias para printar o arquivo
+fun ler(){
+    println("Escreva o diretório em que seu arquivo deve ser lido (exemplo: C:/Users/user1/Documentos/Arquivo.txt) : ")
+    val fileNome = readlnOrNull()
+    if (fileNome!=null){
+        try {
+
+            printarFile(fileNome)
+
+        } catch (e: FileNotFoundException) {
+
+            println("Falha ao ler o arquivo")
+            e.printStackTrace()
+        }
+    }else{
+        ler()
+    }
+}
+
+//printa a matriz e troca os valores repetidos por zero
+fun printarFile(linhaLida: String) {
+    val listaComparacao = mutableListOf<String>()
+    File(linhaLida).forEachLine {
+        val arrayLinha = it.split(",").toMutableList() //obtem a list
+        for(i in arrayLinha.indices) {
+            if(!pertence(arrayLinha[i], listaComparacao)){
+                    listaComparacao.add(arrayLinha[i]) //a lista de comparação receberá o valor
+            }else {
+                listaComparacao.add("0") //a lista de comparação receberá 0
+            }
+        }
+        listaComparacao.add("\n")
+    }
+    println(listaComparacao) //printa a matriz lida
+    printMatrizToInt(listaComparacao) //tirar uma dúvida em relação a questão de ter que imprimir inteiros
+}
+
+//a função verifica se o predicado/string está dentro da lista definida
+fun pertence(pred: String, lista: MutableList<String>): Boolean {
+    for(i in lista.indices){
+        if (pred == lista[i]){
+            return true
+        }else{
+            continue
+        }
+    }
+    return false
+}
+
+//transforma a matriz de String para Int
+fun printMatrizToInt(matriz: MutableList<String>): MutableList<Int> {
+    val matrizInt = mutableListOf<Int>()
+
+    for(i in matriz.indices){
+        if(matriz[i]!="," && matriz[i]!="\n"){
+            matrizInt.add(matriz[i].toInt())
+            print("${ matriz[i].toInt() }, ")
+        }else{
+            println("")
+        }
+    }
+    return matrizInt
+}
+
---
+++ src/main/kotlin/matriz/readme.md
@@ -0,0 +1,14 @@
+# Matrizes
+
+---
+
+### Objetivos:
+
+1. Implemente uma função que leia a matriz de um arquivo
+2. Implementar uma função que identifica os elementos repetidos e transforma-los em zero
+3. Implementar uma função para imprimir na tela uma matriz de inteiros quaisquer
+4. Integrar as funções e otimizar o código em complexidade linearmente
+
+---
+
+
